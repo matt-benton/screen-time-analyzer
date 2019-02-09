b45b9543731f35761d0f6b1004b3b1ece4964604
Matt <mattbenton10@gmail.com>
2019-02-09 13:15:35 -0600

    Replace old font with new one

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,10 +12,6 @@
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
-
     <!-- Icons -->
     <script src="https://unpkg.com/ionicons@4.2.2/dist/ionicons.js"></script>
 
