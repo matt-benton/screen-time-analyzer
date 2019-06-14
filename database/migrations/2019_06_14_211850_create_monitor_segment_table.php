6fa898ba50e517c2b281c68e7416a099857d54e1
Matt <mattbenton10@gmail.com>
2019-06-14 16:38:41 -0500

    Add a pivot table for segments and monitors

---
+++ database/migrations/2019_06_14_211850_create_monitor_segment_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMonitorSegmentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('monitor_segment', function (Blueprint $table) {
+       		$table->increments('id');
+            $table->bigInteger('monitor_id');
+            $table->bigInteger('segment_id');
+            $table->timestamps();
+
+            $table->foreign('monitor_id')->references('id')->on('monitors');
+            $table->foreign('segment_id')->references('id')->on('segments');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('monitor_segment');
+    }
+}
