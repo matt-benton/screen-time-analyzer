9c9a890591748f7a00f5427b2017e4050583e22d
Matt <mattbenton10@gmail.com>
2019-07-05 12:06:29 -0500

    Change the name of the app

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Eye Strain Analyzer
+                   {{ env('APP_NAME') }}
                 </div>
             </div>
         </div>
